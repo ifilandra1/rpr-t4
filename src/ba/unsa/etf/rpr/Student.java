761f139f8a6a93913b637682169c9acec7c90eb3
ifilandra1 <ifilandra1@etf.unsa.ba>
2018-11-05 10:37:43 +0100

    dodani atributi za niz predmeta koje student pohadja, broj indexa, ime i prezime, broj semestra kojeg student pohadja

--- src/ba/unsa/etf/rpr/Student.java
+++ src/ba/unsa/etf/rpr/Student.java
@@ -1,4 +1,9 @@
 package ba.unsa.etf.rpr;
 
 public class Student {
+    private String imeprezime;
+    private int index;
+    private int semestar;
+    private Predmet [] nizpredmeta= new Predmet[10];
+
 }
