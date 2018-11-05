52e93452571d8f36f54e50aa183bcb8b737b914f
ifilandra1 <ifilandra1@etf.unsa.ba>
2018-11-05 10:38:31 +0100

    dodao konstruktor uz objasnjenje kako ce raditi

--- src/ba/unsa/etf/rpr/Student.java
+++ src/ba/unsa/etf/rpr/Student.java
@@ -6,4 +6,11 @@ public class Student {
     private int semestar;
     private Predmet [] nizpredmeta= new Predmet[10];
 
+    Student (String imeprezime, int semestar, int index, Predmet ... nizizbornih) {
+        //pretrazuje niz predmeta iz klase Program  i dodaje svaki obavezni iz svog semestra u svoj niz.
+        //provjerava je li suma ects-ova izbornih + obaveznih veca od 30, ako da, dodaje izborne kod sebe u niz
+        // salje svakom predmetu u nizu string imeprezime i int index;
+    }
+
+
 }
