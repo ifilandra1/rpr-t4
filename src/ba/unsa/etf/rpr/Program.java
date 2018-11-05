da404b519732d3c214c08ea35832e8f1c2f01890
ifilandra1 <ifilandra1@etf.unsa.ba>
2018-11-05 10:35:10 +0100

    dodao dva atributa, niz predmeta i niz studenata

--- src/ba/unsa/etf/rpr/Program.java
+++ src/ba/unsa/etf/rpr/Program.java
@@ -2,7 +2,8 @@ package ba.unsa.etf.rpr;
 
 public class Program {
 
-
+public Student [] nizstudenata=new Student [10];
+public Predmet [] nizpredmeta= new Predmet[10];
     public static void main(String[] args) {
 	// write your code here
     }
