cb046bf0f6a5d9c289450973e6595425be323f6b
ifilandra1 <ifilandra1@etf.unsa.ba>
2018-11-05 20:38:17 +0100

    dodano kratko objasnjenje maina

--- src/ba/unsa/etf/rpr/Program.java
+++ src/ba/unsa/etf/rpr/Program.java
@@ -4,7 +4,8 @@ public class Program {
 
 public Student [] nizstudenata=new Student [10];
 public Predmet [] nizpredmeta= new Predmet[10];
+
     public static void main(String[] args) {
-	// write your code here
+        //pravljenje instanci tipa student i predmet, pohranjivanje referenci na njih u atribute ove klase
     }
 }
