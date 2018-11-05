419c67002a4afdfd11737637fbbedbe0df44e1b8
ifilandra1 <ifilandra1@etf.unsa.ba>
2018-11-05 10:44:13 +0100

    dodan konstrktor uz objasnjenje sta ce se raditi u njemu

--- src/ba/unsa/etf/rpr/Predmet.java
+++ src/ba/unsa/etf/rpr/Predmet.java
@@ -9,6 +9,10 @@ public class Predmet {
     private TipPredmeta tip;
 
 
+    Predmet(String naziv, TipPredmeta tip, int ects, int semestar) {
+        //samo se postavljaju primljeni parametri u novokreirani objekat
+    }
+
 
 
 }
