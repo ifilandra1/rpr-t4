092802ba1382bd85201243f9963c4b3b425f373c
ifilandra1 <ifilandra1@etf.unsa.ba>
2018-11-05 10:41:36 +0100

    dodani atributi za naziv predmeta(string), informacije o studentima koji su upisani na njega(string), broj ects kredita (int), broj semestra u kojem se slusa predmet(int), tip predmeta(enum TipPredmeta - izborni ili obavezni)

--- src/ba/unsa/etf/rpr/Predmet.java
+++ src/ba/unsa/etf/rpr/Predmet.java
@@ -1,4 +1,14 @@
 package ba.unsa.etf.rpr;
 
 public class Predmet {
+
+    public enum TipPredmeta {IZBORNI, OBAVEZNI};
+    private int ects;
+    private String studenti, naziv;
+    private int semestar;
+    private TipPredmeta tip;
+
+
+
+
 }
