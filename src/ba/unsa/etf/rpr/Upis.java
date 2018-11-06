b5941535194f79e8b8fc69046f96b2dd49344f50
ifilandra1 <ooKieGhamohy1>
2018-11-06 18:18:57 +0100

    dodani getteri  i setteri

--- src/ba/unsa/etf/rpr/Upis.java
+++ src/ba/unsa/etf/rpr/Upis.java
@@ -12,5 +12,27 @@ public class Upis {
     }
 
 
+    public PlanStudija getPlan() {
+        return plan;
+    }
 
+    public void setPlan(PlanStudija plan) {
+        this.plan = plan;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Predmet getPredmet() {
+        return predmet;
+    }
+
+    public void setPredmet(Predmet predmet) {
+        this.predmet = predmet;
+    }
 }
