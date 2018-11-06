5336e5871b2c0f92b701b6cba15e786fb437dab8
ifilandra1 <ooKieGhamohy1>
2018-11-06 18:18:43 +0100

    dodana tri atributa: plan studija, student i predmet. Dodan i konstruktor

---
+++ src/ba/unsa/etf/rpr/Upis.java
@@ -0,0 +1,16 @@
+package ba.unsa.etf.rpr;
+
+public class Upis {
+
+
+    PlanStudija plan;
+    Student student;
+    Predmet predmet;
+
+    public Upis (PlanStudija plan, Student s, Predmet p) {
+        this.plan=plan; this.student=s; this.predmet=p;
+    }
+
+
+
+}
