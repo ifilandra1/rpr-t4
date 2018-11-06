67c396b85c0217317ea13252d62b533338512db3
ifilandra1 <ooKieGhamohy1>
2018-11-06 18:12:54 +0100

    dodani getteri i setteri za naziv i ects

--- src/ba/unsa/etf/rpr/Predmet.java
+++ src/ba/unsa/etf/rpr/Predmet.java
@@ -8,5 +8,19 @@ public class Predmet {
     public Predmet(String naziv, int ects) {
         this.naziv=naziv; this.ects=ects;
     }
+    public String getNaziv() {
+        return naziv;
+    }
 
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public int getEcts() {
+        return ects;
+    }
+
+    public void setEcts(int ects) {
+        this.ects = ects;
+    }
 }
