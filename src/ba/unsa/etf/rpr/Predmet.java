47655c6e2925da5cd8e82356349eb9a01733fac7
ifilandra1 <ooKieGhamohy1>
2018-11-06 18:11:15 +0100

    dodao 2 atributa: naziv i broj ects

--- src/ba/unsa/etf/rpr/Predmet.java
+++ src/ba/unsa/etf/rpr/Predmet.java
@@ -2,17 +2,8 @@ package ba.unsa.etf.rpr;
 
 public class Predmet {
 
-    public enum TipPredmeta {IZBORNI, OBAVEZNI};
-    private int ects;
-    private String studenti, naziv;
-    private int semestar;
-    private TipPredmeta tip;
 
-
-    Predmet(String naziv, TipPredmeta tip, int ects, int semestar) {
-        //samo se postavljaju primljeni parametri u novokreirani objekat
-    }
-
-
+    String naziv;
+    int ects;
 
 }
