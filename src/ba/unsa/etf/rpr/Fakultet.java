6f093a52cbc569c3f04f01e1d6774f16198731d1
ifilandra1 <ooKieGhamohy1>
2018-11-06 18:19:29 +0100

    dodana 3 atributa, set predmeta, studenata i upisa

--- src/ba/unsa/etf/rpr/Fakultet.java
+++ src/ba/unsa/etf/rpr/Fakultet.java
@@ -1,9 +1,12 @@
 package ba.unsa.etf.rpr;
 
 
+import java.util.Set;
 
 public class Fakultet {
-
+    Set<Student> studenti;
+    Set<Predmet> predmeti;
+    Set<Upis> upisani;
 
     public static void main(String[] args) {
         //pravljenje instanci tipa student i predmet, pohranjivanje referenci na njih u atribute ove klase
