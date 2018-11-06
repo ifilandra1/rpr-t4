925a1d5fe8ce046c1ac346f5de70db16a7015f00
ifilandra1 <ooKieGhamohy1>
2018-11-06 18:20:03 +0100

    dodane metode za dodavanje studenta i dodavanje predmeta

--- src/ba/unsa/etf/rpr/Fakultet.java
+++ src/ba/unsa/etf/rpr/Fakultet.java
@@ -8,6 +8,15 @@ public class Fakultet {
     Set<Predmet> predmeti;
     Set<Upis> upisani;
 
+    void DodajStudenta(Student s) {
+        studenti.add(s);
+    }
+    void DodajPredmet(Predmet p) {
+        predmeti.add(p);
+    }
+
+
+
     public static void main(String[] args) {
         //pravljenje instanci tipa student i predmet, pohranjivanje referenci na njih u atribute ove klase
     }
